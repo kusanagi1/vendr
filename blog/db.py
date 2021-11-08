fa56c99f16008c769b0b38cee3a809a6ec8df624
kusanagi <rebuiick@yahoo.com>
2021-11-07 20:46:23 -0500

    Fixed the database and its schema

--- blog/db.py
+++ blog/db.py
@@ -6,7 +6,8 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
-def get_db(args):
+def get_db():
+    '''This returns a database connection'''
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -17,7 +18,28 @@ def get_db(args):
     return g.db
 
 def close_db(e=None):
+    '''Closes a database connection if it exists'''
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
+def init_db():
+    """Initialize database when invoked"""
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as file:
+        db.executescript(file.read().decode('utf-8'))
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+@click.command('init_db') 
+@with_appcontext
+def init_db_command():
+    '''purge existing data and create new tables'''
+    init_db()
+    click.echo('Initialized the database.')
+
