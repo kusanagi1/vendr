0fa38b6d912ccbb3b570dbd6b185a56594f6f6e9
kusanagi <rebuiick@yahoo.com>
2021-11-08 22:10:49 -0500

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import blog
+from functools import find_packages, setup
+from os import name
+from sys import version
+
+setup(
+    name='blog',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
